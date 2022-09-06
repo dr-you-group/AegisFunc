446d8131d492e1cdad0230bbd0213fd5ed4b2768
Sungjae Jung <sungjae.2425@gmail.com>
2022-09-06 16:53:11 +0900

    assign geo graph filename

--- R/trans_geo_to_graph.R
+++ R/trans_geo_to_graph.R
@@ -1,16 +1,17 @@
 trans_geo_to_graph <- function(input,
                                 ...) {
   path <- base::getwd()
-  name <- input$geo$name
-  country <- input$geo$country
-  level <- base::as.numeric(input$geo$level)
-  graph_file_path <- base::file.path(path, base::paste0("geo", "_", name, "_", country, "_", level, ".graph"))
+  # name <- input$geo$name
+  # country <- input$geo$country
+  # level <- base::as.numeric(input$geo$level)
+  # graph_file_path <- base::file.path(path, base::paste0("geo", "_", name, "_", country, "_", level, ".graph"))
+  graph_file_path <- base::file.path(path, "geo.graph")
 
-  geo <- get_geo_data(input)
+  # geo <- get_geo_data(input)
 
   # table(rgeos::gIsValid(geo, byid=TRUE))
   # nb <- spdep::poly2nb(geo)
-  geo <- rgeos::gBuffer(geo, width=0, byid=TRUE)
+  geo_m_g <- rgeos::gBuffer(geo_m, width=0, byid=TRUE)
   # table(rgeos::gIsValid(geo, byid=TRUE))
   nb <- spdep::poly2nb(geo)
 
