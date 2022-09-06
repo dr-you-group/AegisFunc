b01253b5c20279697cc45f61b8c03a7e15d0f6eb
Sungjae Jung <sungjae.2425@gmail.com>
2022-09-06 20:42:28 +0900

    add map geo with latlong data

--- R/map_latlong_geo.R
+++ R/map_latlong_geo.R
@@ -7,9 +7,12 @@ map_latlong_geo <- function(input,
   sp::proj4string(latlong) <- sp::proj4string(geo)
 
   geo_map <- sp::over(latlong, geo)
+  # geo_map <- sp::over(latlong, geo)
   geo_map$location_id <- base::seq(1:base::nrow(geo_map))
+  # geo_map$oid <- base::seq(1:base::nrow(geo_map))
 
   geo_map <- sp::merge(geo_map, latlong, by="location_id")
+  # geo_map <- sp::merge(geo_map, geo, by="oid")
 
   geo_map
 }
