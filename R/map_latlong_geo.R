4bdb12fff4a3ece1b44fe00f33901fc85abfff6a
Sungjae Jung <sungjae.2425@gmail.com>
2022-09-06 19:40:24 +0900

    rename variable

--- R/map_latlong_geo.R
+++ R/map_latlong_geo.R
@@ -6,11 +6,11 @@ map_latlong_geo <- function(input,
   sp::coordinates(latlong) <- ~ longitude + latitude
   sp::proj4string(latlong) <- sp::proj4string(geo)
 
-  geo <- sp::over(latlong, geo)
-  geo$location_id <- base::seq(1:base::nrow(geo))
+  geo_map <- sp::over(latlong, geo)
+  geo_map$location_id <- base::seq(1:base::nrow(geo_map))
 
-  geo
+  geo_map
 }
 
 # map_latlong_geo(input)
-# geo <- map_latlong_geo(input)
+# geo_map <- map_latlong_geo(input)
