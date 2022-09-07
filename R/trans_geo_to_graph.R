6e8f272d5f6ae1658b8d27733505e25e8ca5dade
Sungjae Jung <sungjae.2425@gmail.com>
2022-09-07 16:49:13 +0900

    delete unused code

--- R/trans_geo_to_graph.R
+++ R/trans_geo_to_graph.R
@@ -2,13 +2,7 @@ trans_geo_to_graph <- function(input,
                                ...) {
   path <- base::getwd()
   geo <- input$geo
-  # name <- input$geo$name
-  # country <- input$geo$country
-  # level <- base::as.numeric(input$geo$level)
-  # graph_file_path <- base::file.path(path, base::paste0("geo", "_", name, "_", country, "_", level, ".graph"))
   graph_file_path <- base::file.path(path, "geo.graph")
-
-  # geo <- get_geo_data(input)
 
   # table(rgeos::gIsValid(geo, byid=TRUE))
   # nb <- spdep::poly2nb(geo)
