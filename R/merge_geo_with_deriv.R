3fd6fe5acc10c0c2abdb995666a4bffa735a0ff0
Sungjae Jung <sungjae.2425@gmail.com>
2022-09-07 12:57:06 +0900

    add merge geo with deriv function

---
+++ R/merge_geo_with_deriv.R
@@ -0,0 +1,12 @@
+merge_geo_with_deriv <- function(input,
+                                 ...) {
+  geo <- input$geo
+  deriv <- input$deriv
+
+  data <- sp::merge(geo, deriv, by = "oid")
+
+  data
+}
+
+# merge_geo_with_deriv(input)
+# data <- merge_geo_with_deriv(input)
