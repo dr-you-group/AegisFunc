8e180d4a56e7aa1948f47da8c5adce715d922b38
Sungjae Jung <sungjae.2425@gmail.com>
2022-09-20 13:57:40 +0900

    bug fix

--- R/calculate_count_with_geo_oid.R
+++ R/calculate_count_with_geo_oid.R
@@ -2,10 +2,13 @@ calculate_count_with_geo_oid <- function(input,
                                      ...) {
   table <- input$table
 
+  table$target_count[is.na(table$target_count)] <- 0
+  table$outcome_count[is.na(table$outcome_count)] <- 0
+
   table <- stats::aggregate(
     cbind(
       target_count, outcome_count
-    ) ~ oid, age_category, sex_category,
+    ) ~ oid + age_category + sex_category,
     data = table,
     FUN = base::sum
   )
--- R/map_latlong_geo.R
+++ R/map_latlong_geo.R
@@ -7,7 +7,8 @@ map_latlong_geo <- function(input,
   sp::proj4string(latlong) <- sp::proj4string(geo)
 
   geo_map <- sp::over(latlong, geo)
-  geo_map$location_id <- base::seq(1:base::nrow(geo_map))
+  # geo_map$location_id <- base::seq(1:base::nrow(geo_map))
+  geo_map <- base::cbind(geo_map, latlong)
 
   geo_map
 }
