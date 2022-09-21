9e4afe0e5b7d08f676cd97dd04946c633395d2e6
Sungjae Jung <sungjae.2425@gmail.com>
2022-09-21 12:56:06 +0900

    update disease cluster function

--- R/calculate_count_with_geo_oid.R
+++ R/calculate_count_with_geo_oid.R
@@ -2,15 +2,20 @@ calculate_count_with_geo_oid <- function(input,
                                          ...) {
   table <- input$table
 
-  table$target_count[is.na(table$target_count)] <- 0
+  table$target_count[is.na(table$target_count)] <- 1
   table$outcome_count[is.na(table$outcome_count)] <- 0
+  table$latitude[is.na(table$latitude)] <- table$lat[is.na(table$latitude)]
+  table$longitude[is.na(table$longitude)] <- table$long[is.na(table$longitude)]
+  table$age_category[is.na(table$age_category)] <- 0
+  table$sex_category[is.na(table$sex_category)] <- 0
 
-  table <- stats::aggregate(
+  table_a <- stats::aggregate(
     cbind(
       target_count, outcome_count
     ) ~ oid + latitude + longitude + age_category + sex_category,
     data = table,
-    FUN = base::sum
+    FUN = base::sum,
+    na.action = na.pass
   )
 
   table
--- R/map_latlong_geo.R
+++ R/map_latlong_geo.R
@@ -9,10 +9,13 @@ map_latlong_geo <- function(input,
   geo_map <- sp::over(latlong, geo)
   # geo_map$location_id <- base::seq(1:base::nrow(geo_map))
   geo_map <- base::cbind(geo_map, latlong)
+  geo_map <- base::merge(geo, geo_map, all.x=T)
 
   for (i in 1:base::max(base::unique(geo_map$oid))) {
     geo_map[(geo_map$oid == i), c("latitude", "longitude")] <- geo_map[(geo_map$oid == i), c("latitude", "longitude")][1, c("latitude", "longitude")]
   }
+
+  geo_map <- geo_map[base::order(geo_map$oid),]
 
   geo_map
 }
