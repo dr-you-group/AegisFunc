79f9d6d6b5d14761264c4fc36bc65a338f01bc61
Sungjae Jung <sungjae.2425@gmail.com>
2022-10-27 01:33:46 +0900

    update for spatio-temporal function

--- R/calculate_disease_cluster.R
+++ R/calculate_disease_cluster.R
@@ -13,38 +13,50 @@ calculate_disease_cluster <- function(model = "spatial", table,
   model <- model
   table <- table
 
-  results <- NULL
-  stats <- NULL
-  arranged_table <- NULL
+  result <- base::list()
 
   if(model == "spatial") {
-    results <- SpatialEpi::kulldorff(
-      geo = SpatialEpi::latlong2grid(table[, c("longitude", "latitude")]),
-      cases = base::tapply(table$outcome_count, table$oid, sum),
-      population = base::tapply(table$target_count, table$oid, sum),
-      expected.cases = table$expected,
-      pop.upper.bound = 0.1,
-      n.simulations = 999,
-      alpha.level = 0.05,
-      plot = FALSE
-    )
-
-    stats <- base::as.data.frame(results$most.likely.cluster)
-    for (i in 1:base::length(results$secondary.clusters)) {
-      stats <- base::rbind(stats, base::as.data.frame(results$secondary.clusters[[i]]))
-    }
-
-    names(stats) <- c("idx", "population", "number_of_cases", "expected_cases", "smr", "log_likelihood_ratio", "monte_carlo_rank", "p_value")
-
-    empty <- base::data.frame(idx = base::seq(1, base::nrow(table), 1))
-    stats <- base::merge(stats, empty, by = "idx", all.y = TRUE)
-
-    arranged_table <- table
-    arranged_table$indicator <- NA
-    arranged_table[!is.na(stats$log_likelihood_ratio), ]$indicator <- base::seq(1, base::sum(!is.na(stats$log_likelihood_ratio)), 1)
+    result <- run_kulldorff(table)
   } else if (model == "spatio-temporal") {
-    # code here
+    years <- sort(unique(table$cohort_start_year))
+
+    for(i in 1:length(years)) {
+      idx <- table$cohort_start_year == years[i]
+
+      result[years[i]] <- run_kulldorff(table[idx,])
+    }
   }
+
+  output <- result
+
+  output
+}
+
+run_kulldorff <- function(table) {
+  results <- SpatialEpi::kulldorff(
+    geo = SpatialEpi::latlong2grid(table[, c("longitude", "latitude")]),
+    cases = base::tapply(table$outcome_count, table$oid, sum),
+    population = base::tapply(table$target_count, table$oid, sum),
+    expected.cases = table$expected,
+    pop.upper.bound = 0.1,
+    n.simulations = 999,
+    alpha.level = 0.05,
+    plot = FALSE
+  )
+
+  stats <- base::as.data.frame(results$most.likely.cluster)
+  for (i in 1:base::length(results$secondary.clusters)) {
+    stats <- base::rbind(stats, base::as.data.frame(results$secondary.clusters[[i]]))
+  }
+
+  names(stats) <- c("idx", "population", "number_of_cases", "expected_cases", "smr", "log_likelihood_ratio", "monte_carlo_rank", "p_value")
+
+  empty <- base::data.frame(idx = base::seq(1, base::nrow(table), 1))
+  stats <- base::merge(stats, empty, by = "idx", all.y = TRUE)
+
+  arranged_table <- table
+  arranged_table$indicator <- NA
+  arranged_table[!is.na(stats$log_likelihood_ratio), ]$indicator <- base::seq(1, base::sum(!is.na(stats$log_likelihood_ratio)), 1)
 
   output <- base::list()
   output$model <- model
--- R/calculate_disease_map.R
+++ R/calculate_disease_map.R
@@ -15,31 +15,43 @@ calculate_disease_map <- function(model = "spatial", table, graph_file_path,
   table <- table
   graph_file_path <- graph_file_path
 
-  results <- NULL
-  stats <- NULL
-  arranged_table <- NULL
+  result <- base::list()
 
   if(model == "spatial") {
-    table$oid2 <- table$oid
-
-    results <- INLA::inla(
-      formula = outcome_count ~ 1 +
-        f(oid, model = "iid") +
-        f(oid2, model = "bym2", graph = graph_file_path, adjust.for.con.comp = TRUE),
-      family = "poisson",
-      data = table,
-      E = expected,
-      control.predictor = base::list(compute = TRUE)
-    )
-
-    stats <- base::list()
-    stats$rr_mean <- results$summary.fitted.values$mean
-
-    arranged_table <- table
-    arranged_table$indicator <- stats$rr_mean
+    result <- run_inla(table, graph_file_path)
   } else if (model == "spatio-temporal") {
-    # code here
+    years <- sort(unique(table$cohort_start_year))
+
+    for(i in 1:length(years)) {
+      idx <- table$cohort_start_year == years[i]
+
+      result[years[i]] <- run_inla(table[idx,], graph_file_path)
+    }
   }
+
+  output <- result
+
+  output
+}
+
+run_inla <- function(table, graph_file_path) {
+  table$oid2 <- table$oid
+
+  results <- INLA::inla(
+    formula = outcome_count ~ 1 +
+      f(oid, model = "iid") +
+      f(oid2, model = "bym2", graph = graph_file_path, adjust.for.con.comp = TRUE),
+    family = "poisson",
+    data = table,
+    E = expected,
+    control.predictor = base::list(compute = TRUE)
+  )
+
+  stats <- base::list()
+  stats$rr_mean <- results$summary.fitted.values$mean
+
+  arranged_table <- table
+  arranged_table$indicator <- stats$rr_mean
 
   output <- base::list()
   output$model <- model
